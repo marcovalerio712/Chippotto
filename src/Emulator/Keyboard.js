dd099677f168a3856923fafe60ed4cf2e7d7ac38
marcovalerio712 <marcovalerio71299@gmail.com>
2022-05-26 09:37:41 +0200

    Inseriti Eventi di pressione di tasti sulla tastiera

--- src/Emulator/Keyboard.js
+++ src/Emulator/Keyboard.js
@@ -1,15 +1,25 @@
 class KeyBoard{
 
     constructor(btnList){
-        this.buffer = []
+        this.buffer = new Array(16).fill(false)
+        this.lastPressedButton = null
         console.log(btnList)
 
         btnList.forEach(element => {
-            element.addEventListener('click', ()=>{
-                console.log(element.innerHTML)
-            })
+            let elementValue = parseInt(element.innerHTML, 16)
+            element.onpointerdown = () => {
+                this.lastPressedButton = elementValue
+                this.buffer[elementValue] = true
+                console.log(element.innerHTML + " down")
+            }
+            element.onpointerup = ()=> {
+                this.buffer[elementValue] = false
+                console.log(element.innerHTML + " up")
+            }
+            
         });
-    }
+        
+    }  
 
 }
 
